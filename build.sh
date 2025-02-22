872d5e994fd41bbeba09acdc7600703025d90f77
lazypwny751 <testfordnshijacking@gmail.com>
2025-02-22 23:59:41 +0300

    upgrade fontsize

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ done
 
 octave "${source:-logo.m}"
 convert "${file:-pearl.png}" -alpha set -channel A -evaluate multiply 0.95 \
-    -gravity center -fill white -pointsize 220 -stroke black -strokewidth 3 \
+    -gravity center -fill white -pointsize 260 -stroke black -strokewidth 3 \
     -font "moonless.otf" -annotate +90+245 "Pearl Language" "${out:-pearl_logo.png}"
 [ -f "${file:-pearl.png}" ] && rm -vf "${file:-pearl.png}"
 [ -f "${out:-pearl_logo.png}" ] && echo "logo: \"${out:-pearl_logo.png}\""
